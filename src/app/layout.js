2db3e4770cdf7632ac145769ae98fcdc0b06051a
MiltogroDEV <emilton.code@gmail.com>
2025-07-08 00:11:00 -0300

    Configuring Menu Component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Menu from "@/components/menu/Menu";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
